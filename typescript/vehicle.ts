c8c306de29176e4efb75a223463186118e522e95
Johan Lorentzon <johan.lorentzon@layer10.se>
2024-04-18 14:02:37 +0200

    Add Typescript

---
+++ typescript/vehicle.ts
@@ -0,0 +1,17 @@
+
+export class Vehicle {
+}
+
+class Car extends Vehicle {
+    getType() {
+        return 'Car';
+    }
+}
+
+class Motorbike extends Vehicle {
+    getType() {
+        return 'Motorbike';
+    }
+}
+
+export { Car, Motorbike };
